9e1a789592a0e93472fff2f8b48022a3c0eb9842
SumraYasmin <sumrayasmin9@gmail.com>
2018-08-08 11:05:25 +0500

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
 
+namespace :admin do
+	resources :users do
+		member do
+			get 'change-status'
+		end
+	end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
